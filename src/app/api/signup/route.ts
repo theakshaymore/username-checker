331a30cabf75889a7ba7214cd2b01da370cf43ce
Akshay More <moreakshay725@gmail.com>
2026-05-21 21:53:39 +0530

    feat: fix user verification check in signup route to await database query

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -9,10 +9,20 @@ export async function POST(request: Request) {
   try {
     const { username, email, password } = await request.json();
 
-    const existingUserVerfiedByUsername = UserModel.findOne({
+    const existingUserVerfiedByUsername = await UserModel.findOne({
       username,
       isVerified: true,
     });
+
+    if (existingUserVerfiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken.",
+        },
+        { status: 400 },
+      );
+    }
   } catch (error) {
     console.error("Error in signup route:", error);
     return Response.json(
