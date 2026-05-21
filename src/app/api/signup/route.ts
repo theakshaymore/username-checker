b766cdb424c6a73d682af517334e0e9c01fc8890
Akshay More <moreakshay725@gmail.com>
2026-05-21 17:18:17 +0530

    feat: implement POST method for signup route with error handling

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,2 +1,23 @@
+import bcrypt from "bcryptjs";
+
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/user";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    //
+  } catch (error) {
+    console.error("Error in signup route:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "An error occurred during signup. Please try again later.",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
